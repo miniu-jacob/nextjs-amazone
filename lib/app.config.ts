508f048c2301515d5670baf1a30f540219276f0c
Jacob CHUNG <miniu.media@gmail.com>
2025-01-07 16:17:48 +0700

    05-03-removed-clogs

--- lib/app.config.ts
+++ lib/app.config.ts
@@ -2,16 +2,11 @@
 
 import { getEnv } from "./get-env";
 
-const appConfig = () => {
-  // const MONGODB_URI = getEnv("MONGODB_URI"); // MONGODB_URI 가져오기
-  // console.log("getEnv('MONGODB_URI'):", MONGODB_URI); // 디버깅: 값 출력
-
-  return {
-    APP_NAME: getEnv("NEXT_PUBLIC_APP_NAME", "MiniMarket"),
-    APP_SLOGAN: getEnv("NEXT_PUBLIC_APP_SLOGAN", "Your online miniu market"),
-    APP_DESCRIPTION: getEnv("NEXT_PUBLIC_APP_DESCRIPTION", "MINIU Market for selling and buying productsYour online mini market"),
-    MONGODB_URI: getEnv("MONGODB_URI"),
-  };
-};
+const appConfig = () => ({
+  APP_NAME: getEnv("NEXT_PUBLIC_APP_NAME", "MiniMarket"),
+  APP_SLOGAN: getEnv("NEXT_PUBLIC_APP_SLOGAN", "Your online miniu market"),
+  APP_DESCRIPTION: getEnv("NEXT_PUBLIC_APP_DESCRIPTION", "MINIU Market for selling and buying productsYour online mini market"),
+  MONGODB_URI: getEnv("MONGODB_URI"),
+});
 
 export const config = appConfig();
--- lib/db/seed.ts
+++ lib/db/seed.ts
@@ -4,24 +4,18 @@ import data from "../data";
 import { connectToDatabase } from ".";
 import Product from "./models/product.model";
 import { cwd } from "process";
-import { clog } from "../jlogger";
 import { loadEnvConfig } from "@next/env";
 
 // (1). .env.local 파일을 현재 작업 디렉토리에서 로드하여 환경 변수를 설정한다.
 loadEnvConfig(cwd());
 
-console.log("MONGODB_URI (process.env): ", process.env.MONGODB_URI);
-
 const main = async () => {
   try {
-    clog.log("[seed.ts] main() started");
-
     // (2). 미리 정의된 상품 정보(data)를 불러온다.
     const { products } = data;
 
     // (3). DB 에 연결한다. connectToDatabase 함수에서 이미 MONGODB_URI 환경 변수를 사용하기 때문에 인자가 필요없다.
     await connectToDatabase();
-    clog.info("[seed.ts] Connected to database");
 
     // (4). 기존 데이터 삭제 및 새 데이터 삽입
     await Product.deleteMany();
@@ -33,7 +27,7 @@ const main = async () => {
     });
     process.exit(0);
   } catch (error) {
-    clog.error(error);
+    console.error(error);
     throw new Error("Failed to seed database.");
   }
 };
