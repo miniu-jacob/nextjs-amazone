f7bef3051e2819387e4e8f88a0b73e50bb43a39d
Jacob CHUNG <miniu.media@gmail.com>
2025-02-14 13:31:29 +0700

    36-03. restore next.config.ts i18n setting

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import withNextIntl from "next-intl/plugin";
 
 // const nextConfig: NextConfig = {    <--- 기존
 const nextConfig: NextConfig = withNextIntl()({
-  i18n: {
-    locales: ["en-US", "ko-KR", "vi-VN"],
-    defaultLocale: "en-US",
-    localeDetection: false, // 자동 감지 비활성화
-  },
   images: {
     remotePatterns: [
       {
